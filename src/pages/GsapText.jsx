ca7d4f37c2d7b4224f4f75a8364a52700aad7a42
Farok Hossain <farokafs7@gmail.com>
2024-03-25 16:50:14 +0600

    updated

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,5 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to("#text", {
+      ease: "power1.inOut",
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.fromTo(
+      ".sara",
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        stagger: 0.3,
+      }
+    );
+  }, []);
 
   return (
     <main>
@@ -7,24 +31,24 @@ const GsapText = () => {
         GsapText
       </h1>
 
-      <p className="mt-5 text-gray-500 para">
+      <p className="mt-5 text-gray-500 sara">
         We can use same method like <code>gsap.to()</code>,{" "}
         <code>gsap.from()</code>, <code>gsap.fromTo()</code> and{" "}
         <code>gsap.timeline()</code> to animate text.
       </p>
 
-      <p className="mt-5 text-gray-500 para">
+      <p className="mt-5 text-gray-500 sara">
         Using these methods we can achieve various text animations and effects
         like fade in, fade out, slide in, slide out, and many more.
       </p>
 
-      <p className="mt-5 text-gray-500 para">
+      <p className="mt-5 text-gray-500 sara">
         For more advanced text animations and effects, you can explore the GSAP
         TextPlugin or other third-party libraries that specialize in text
         animations.
       </p>
 
-      <p className="mt-5 text-gray-500 para">
+      <p className="mt-5 text-gray-500 sara">
         Read more about the{" "}
         <a
           href="https://greensock.com/docs/v3/Plugins/TextPlugin"
